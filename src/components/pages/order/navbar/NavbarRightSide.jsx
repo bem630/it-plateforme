025b0940e91e60c3edc2b7455365624037a2c05e
Bah Elhadj Mamadou <mamadou.bahelhadj@etudiant-fst.utm.tn>
2024-07-21 14:27:06 +0100

    ✨ F01:  Navbar > Create NavbarRightSide

--- src/components/pages/order/navbar/NavbarRightSide.jsx
+++ src/components/pages/order/navbar/NavbarRightSide.jsx
@@ -1,16 +1,29 @@
 import React from 'react'
 import {  NavLink } from 'react-router-dom';
 import styled from 'styled-components';
+import { FaCalendarAlt, FaGraduationCap, FaHome, FaInfoCircle } from "react-icons/fa";
 
-export default function NavbarRightSide() {
+export default function NavbarRightSide({ isOpen, toggleMenu }) {
   return (
-    <NavbarRightSideStyled>
-        <li className="nav-item"><NavLink className="navbar-links" to='/'>Accueil</NavLink></li>
-        <li className="nav-item"><NavLink className="navbar-links" to='/about'>Apropos</NavLink></li>
-        <li className="nav-item"><NavLink className="navbar-links" to='/formation'>Formations</NavLink></li>
-        <li className="nav-item"><NavLink className="navbar-links" to='/evenement'>Evenements</NavLink></li>
-        <li className="nav-item"><NavLink className="navbar-links" to='/blogs'>Blogs</NavLink></li>
-        <li className="nav-item"><NavLink className="register-button" to='/signUp'>S'inscrire</NavLink></li>
+    <NavbarRightSideStyled isOpen={isOpen}>
+        <li className='nav-item'>
+          <NavLink className="navbar-links" to='/'>Accueil</NavLink>
+        </li>
+        <li className='nav-item'>
+          <NavLink className="navbar-links" to='/about'>Apropos</NavLink>
+        </li>
+        <li className='nav-item'>
+          <NavLink className="navbar-links" to='/formation'>Formations</NavLink>
+        </li>
+        <li className='nav-item'>
+          <NavLink className="navbar-links" to='/evenement'>Evenements</NavLink>
+        </li>
+        <li className='nav-item'>
+          <NavLink className="navbar-links" to='/blogs'>Blogs</NavLink>
+        </li>
+        <li className='nav-item'>
+          <NavLink className="register-button" to='/signUp'>S'inscrire</NavLink>
+        </li>
     </NavbarRightSideStyled>
   )
 }
@@ -23,12 +36,14 @@ const NavbarRightSideStyled = styled.ul`
   font-size: 1.2rem;
   .nav-item {
     margin: 0 10px;
-    color: black;
 
     .navbar-links {
         color: #000;
         text-decoration: none;
-        transition: 0.3s ease-in-out;      
+        transition: 0.3s ease-in-out; 
+        &:hover {
+            color: #007bff;
+        }      
     }
     .register-button {
         font-size: 16px;
@@ -46,6 +61,25 @@ const NavbarRightSideStyled = styled.ul`
         &:hover {
         background-color: #0056b3;
 }
+    }
+  }
+  @media (max-width: 968px) {
+    position: fixed;
+    top: 12vh;
+    top: 80px;
+    left: 0;
+    right: 0;
+    background-color: #fff;
+    height: 100vh;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    transition: transform 0.3s ease-in-out;
+    transform: ${({ isOpen }) => (isOpen ? 'translateX(0)' : 'translateX(-100%)')};
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+
+    .nav-item {
+      margin: 20px 0;
     }
   }
 `;
