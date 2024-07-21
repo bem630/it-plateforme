83f0894727ab108ffcc7d1d76a18cfff0dacd225
Bah Elhadj Mamadou <mamadou.bahelhadj@etudiant-fst.utm.tn>
2024-07-21 14:32:38 +0100

    ✨ F01:  Main > Create Formation

--- src/components/pages/order/main/formation/Formation.jsx
+++ src/components/pages/order/main/formation/Formation.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function Formation() {
   return (
-    <div>Formation</div>
+    <div>
+        <h2 className='title-category-formation'>Formations populaires</h2>
+    </div>
   )
 }
+
