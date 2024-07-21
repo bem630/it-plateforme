83d039548951191d0b860dafbeb7665c8586bae4
Bah Elhadj Mamadou <mamadou.bahelhadj@etudiant-fst.utm.tn>
2024-07-21 14:36:12 +0100

    ✨ F01:  Add Footer in routing system

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import Navbar from './components/pages/order/navbar/Navbar'
 import Home from './components/pages/order/main/home/Home.jsx'
 import ErrorPage from './components/pages/error/ErrorPage.jsx'
+import Footer from './components/pages/order/footer/Footer.jsx'
 function App() {
 
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" exact element={<Home/>}/>
         <Route path="*"  element={<ErrorPage/>}/>
       </Routes>
+      <Footer/>
     </>
   )
 }
