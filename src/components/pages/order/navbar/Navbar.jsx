0228f395be7e10c54ca7a7ff48317bf38fec7590
Bah Elhadj Mamadou <mamadou.bahelhadj@etudiant-fst.utm.tn>
2024-07-21 14:28:45 +0100

    ✨ F01:  Main> Create Navbar

--- src/components/pages/order/navbar/Navbar.jsx
+++ src/components/pages/order/navbar/Navbar.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from '../../../reusable-ui/Logo'
 import NavbarRightSide from './NavbarRightSide'
 import styled from 'styled-components';
+import { FaBars, FaTimes } from "react-icons/fa";
 
 export default function Navbar() {
+  //state
+  const [isOpen, setIsOpen] = useState(false);
+  //comportement
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+  //affichage
   return (
     <NavbarStyled>
         <Logo className="logo-order-page"/>
-        <NavbarRightSide/>
+        <MenuIcon onClick={toggleMenu}> {isOpen ? <FaTimes /> : <FaBars />}</MenuIcon>
+        <NavbarRightSide isOpen={isOpen}/>
     </NavbarStyled>
   )
 }
@@ -15,15 +24,29 @@ const NavbarStyled = styled.nav`
   /*border: 1px solid blue;*/
   background-color: #fff;
   height: 12vh;
+  height: 80px;
   display: flex;
   justify-content: space-between;
-  padding: 0 2%;
+  padding: 0 2rem;
   align-items: center;
-  margin: 0;
   position: fixed;
   top: 0;
   left: 0;
   right: 0;
   z-index: 1000;
   box-sizing: border-box;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  @media (max-width: 968px) {
+    padding: 0 1;
+  }
+`;
+
+const MenuIcon = styled.div`
+  display: none;
+  font-size: 1.8rem;
+  cursor: pointer;
+
+  @media (max-width: 968px) {
+    display: block;
+  }
 `;
